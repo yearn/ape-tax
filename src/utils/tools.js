ac8c19a02ff1229c628f250869483bd23ed17c4a
Thomas Bouder <tbouder@protonmail.com>
2021-08-08 12:56:13 +0200

    🔥 Burning unused code

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -29,9 +29,3 @@ export async function	fetchYearnVaults() {
 	}
 	return null;
 }
-
-export async function asyncForEach(array, callback) {
-	for (let index = 0; index < array.length; index++) {
-		await callback(array[index], index, array);
-	}
-}
