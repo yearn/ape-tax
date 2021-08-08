0703374d623e6d363b3b200edd5f4bfc722588c2
Thomas Bouder <tbouder@protonmail.com>
2021-08-08 12:47:44 +0200

    ✨ Vaults withdraw alert

--- src/Home.vue
+++ src/Home.vue
@@ -2,9 +2,36 @@
 #home
   h1.title.is-3.is-hidden-mobile Experimental Experiments Registry
   h1.title.is-3.is-hidden-tablet Ex<sup>2</sup> Registry
-  div.container.is-max-desktop.warning.is-size-7-mobile ⚠️ <strong>WARNING</strong> this experiments are experimental. They are extremely risky and will probably be discarded when the test is over. There's a good chance that you can lose your funds. If you choose to proceed, do it with extreme caution.
-  div.columns
-    div.column.is-one-third.is-half-mobile
+  
+  //---------------------------------------------------------------------------
+  //- Disclaimer message. For you in the code. This project is amazing because
+  //- it's the frontline of the vaults and the strategies in DEFI. But the
+  //- front line could have some issue when it come to survival. Have fun !
+  //---------------------------------------------------------------------------
+  div.max-w-5xl.warning.is-size-7-mobile ⚠️ <strong>WARNING</strong> this experiments are experimental. They are extremely risky and will probably be discarded when the test is over. There's a good chance that you can lose your funds. If you choose to proceed, do it with extreme caution.
+  
+  //---------------------------------------------------------------------------
+  //- Inactive vault alert banner. This will only be displayed if the user has
+  //- funds in a `withdraw` vault.
+  //---------------------------------------------------------------------------
+  div(v-show="vaultsInactive.length")
+    div.max-w-5xl.withdraw.is-size-7-mobile.pb-0 🚨 <strong>YOU ARE USING DEPRECATED VAULTS</strong> You have funds in deprecated vaults. Theses vaults are no longer generating any profit and are now an image from the past. Please remove your funds from these vaults.
+      div.mt-4
+        ul.grid.grid-cols-2
+          li.vaultLine.xs-col-span-1.col-span-2(v-for="vault in vaultsInactive")
+            a.is-size-6.is-size-7-mobile(class="links" :href="'/' + vault.URL")
+              <div class="vault">
+                span( class="vaultLogo" v-for="letter in splitLogo(vault.LOGO)")
+                  <span>{{ letter }}</span>
+                <span class="text">{{ vault.TITLE }}</span> 
+              </div>
+  
+  //---------------------------------------------------------------------------
+  //- This section will display the vaults in two columns. The left column will
+  //- display the 'normal' vaults, and the right one the 'weird' vaults.
+  //---------------------------------------------------------------------------
+  section.max-w-5xl.grid.grid-cols-2
+    div.xs-col-span-1.col-span-2.mb-4
       h2(v-show="experimentalVaultsActive.length || experimentalVaultsOther.length").title.is-size-4.is-size-6-mobile 🚀 Experimental
       ul
         li(class="vaultLine" v-for="vault in [...experimentalVaultsActive, ...experimentalVaultsOther]")
@@ -15,7 +42,7 @@
               <span class="text">{{ vault.TITLE }}</span>
             </div>
           status-tag(:status="vault.VAULT_STATUS")
-    div.column.is-one-third.is-half-mobile
+    div.xs-col-span-1.col-span-2
       h2(v-show="weirdVaultsActive.length").title.is-size-4.is-size-6-mobile 🧠 Weird
       ul
         li(class="vaultLine" v-for="vault in [...weirdVaultsActive, ...weirdVaultsOther]")
@@ -26,18 +53,6 @@
               <span class="text">{{ vault.TITLE }}</span> 
             </div>
           status-tag(:status="vault.VAULT_STATUS")
-    div.column.is-one-third.is-half-mobile
-      h2(v-show="vaultsInactive.length").title.is-size-4.is-size-6-mobile 🚨 Withdraw
-      ul
-        li(class="vaultLine" v-for="vault in vaultsInactive")
-          a.is-size-6.is-size-7-mobile(class="links" :href="'/' + vault.URL")
-            <div class="vault">
-              span( class="vaultLogo" v-for="letter in splitLogo(vault.LOGO)")
-                <span>{{ letter }}</span>
-              <span class="text">{{ vault.TITLE }}</span> 
-            </div>
-          status-tag(:status="vault.VAULT_STATUS")
-
 </template>
 
 <script>
@@ -48,9 +63,7 @@ const splitter = new GraphemeSplitter();
 
 export default {
   name: "Home",
-  components: {
-    StatusTag,
-  },
+  components: {StatusTag},
   props: ["allConfig", "chainId", 'currentAccount'],
   data() {
     return {
@@ -75,13 +88,10 @@ export default {
       const items = this.items;
 
       const result = items
-        .filter((item) => item.CHAIN_ID === this.chainId)
-        .filter(
-          (item) =>
-            item.VAULT_TYPE === "experimental" && (item.VAULT_STATUS === "active" || item.VAULT_STATUS === "new")
-        )
-        .slice()
-        .reverse();
+        .filter((item) =>
+          item.CHAIN_ID === this.chainId &&
+          item.VAULT_TYPE === "experimental" && (item.VAULT_STATUS === "active" || item.VAULT_STATUS === "new")
+        ).reverse();
 
       return result;
     },
@@ -101,13 +111,10 @@ export default {
       const items = this.items;
 
       const result = items
-        .filter((item) => item.CHAIN_ID === this.chainId)
-        .filter(
-          (item) =>
-            item.VAULT_TYPE === "weird" && (item.VAULT_STATUS === "active" || item.VAULT_STATUS === "new")
-        )
-        .slice()
-        .reverse();
+        .filter((item) =>
+          item.CHAIN_ID === this.chainId &&
+          item.VAULT_TYPE === "weird" && (item.VAULT_STATUS === "active" || item.VAULT_STATUS === "new")
+        ).reverse();
 
       return result;
     },
@@ -115,14 +122,11 @@ export default {
       const items = this.items;
 
       const result = items
-        .filter((item) => item.CHAIN_ID === this.chainId)
-        .filter(
-          (item) =>
-            item.VAULT_TYPE === "weird" &&
-            item.VAULT_STATUS === "endorsed"
-        )
-        .slice()
-        .reverse();
+        .filter((item) =>
+          item.CHAIN_ID === this.chainId &&
+          item.VAULT_TYPE === "weird" &&
+          item.VAULT_STATUS === "endorsed"
+        ).reverse();
 
       return result;
     },
@@ -131,7 +135,7 @@ export default {
     const items = this.items;
     const result = items.filter(item => (item.CHAIN_ID === this.chainId) && (item.VAULT_STATUS === 'withdraw'))
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    const currentAccount = '0xeafb3ee25b5a9a1b35f193a4662e3bdba7a95beb' || (await provider.listAccounts())[0];
+    const currentAccount = (await provider.listAccounts())[0];
 
     const promises = await Promise.all(result.map(async ({VAULT_ADDR}) => {
       const	vaultContract = new ethers.Contract(VAULT_ADDR, ['function balanceOf(address) view returns (uint256)'], provider);
@@ -162,6 +166,30 @@ div.warning {
   padding: 1rem;
   background-color: #fff257;
 }
+div.withdraw {
+  margin-top: 1rem !important;
+  margin-bottom: 2rem !important;
+  margin-left: 0 !important;
+  padding: 1rem;
+  background-color: #EF4444;
+  color: white !important;
+}
+div.withdraw strong{
+  color: white !important;
+}
+div.withdraw > .columns {
+  margin-top: 0rem;
+}
+div.withdraw .text{
+  color: white !important;
+}
+div.withdraw .vaultLogo{
+  color: white !important;
+}
+div.withdraw a.links:hover span.text {
+  border-bottom: 1px dotted white !important;
+}
+
 div.endorsed {
   color: white !important;
   background-color: #0657F9 !important;
@@ -215,5 +243,9 @@ a.links:hover span.text {
 }
 .vault > .vaultLogo:first-child {
   text-align: left;
+}
+.grid > .vaultLine {
+  margin-top: 0;
+  margin-bottom: 1rem;
 }
 </style>
