57ea3644e9a03ce88297dcbc2e30ba0b7ebf9559
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-07-30 21:03:16 +0700

    feat: Add fetch util and hook for improved data fetching

---
+++ hooks/useFetch.ts
@@ -0,0 +1,37 @@
+import useSWR from 'swr';
+// import * as Sentry from '@sentry/nextjs';
+import {baseFetcher} from '@yearn-finance/web-lib/utils/fetchers';
+
+import type {SWRResponse} from 'swr';
+import type {z} from 'zod';
+
+type TUseZodProps<T> = {
+	endpoint: string | null;
+	schema: z.ZodSchema;
+	config?: Parameters<typeof useSWR<T>>[2];
+}
+
+export function useFetch<T>({endpoint, schema, config}: TUseZodProps<T>): SWRResponse<T> & {isSuccess: boolean} {
+	const result = useSWR<T>(endpoint, baseFetcher, {revalidateOnFocus: false, ...config});
+
+	if (!result.data || result.isLoading || result.isValidating) {
+		return {...result, isSuccess: false};
+	}
+
+	if (result.error) {
+		console.error(endpoint, result.error);
+		// Sentry.captureException(result.error, {tags: {endpoint}});
+		return {...result, isSuccess: false};
+	}
+
+
+	const parsedData = schema.safeParse(result.data);
+
+	if (!parsedData.success) {
+		console.error(endpoint, parsedData.error);
+		// Sentry.captureException(parsedData.error, {tags: {endpoint}});
+		return {...result, isSuccess: false};
+	}
+
+	return {...result, data: parsedData.data, isSuccess: true};
+}
---
+++ utils/fetch.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+// import * as Sentry from '@sentry/nextjs';
+import {serialize} from '@wagmi/core';
+
+import type {AxiosRequestConfig} from 'axios';
+import type {z} from 'zod';
+
+type TFetchProps = {
+	endpoint: string | null;
+	schema: z.ZodSchema;
+	config?: AxiosRequestConfig<unknown>;
+}
+
+export type TFetchReturn<T> = Promise<{data: T | null, error?: Error}>
+
+export async function fetch<T>({endpoint, schema, config}: TFetchProps): TFetchReturn<T> {
+	if (!endpoint) {
+		return {data: null, error: new Error('No endpoint provided')};
+	}
+
+	try {
+		const {data} = await axios.get<T>(endpoint, config);
+
+		if (!data) {
+			return {data: null, error: new Error('No data')};
+		}
+
+		const parsedData = schema.safeParse(data);
+
+		if (!parsedData.success) {
+			console.error(endpoint, parsedData.error);
+			// Sentry.captureException(parsedData.error, {tags: {endpoint}});
+			return {data, error: parsedData.error};
+		}
+
+		return {...data, data: parsedData.data};
+	} catch (error) {
+		console.error(endpoint, error);
+		// Sentry.captureException(error, {tags: {endpoint}});
+		if (error instanceof Error) {
+			return {data: null, error};
+		}
+		return {data: null, error: new Error(serialize(error))};
+	}
+}
