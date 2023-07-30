b2bd56df94072612e79ff480208e04ffb5ee290b
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-07-30 15:04:40 +0700

    refactor: Comment unused function, removed older commented function

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -67,69 +67,14 @@ export async function fetchBlockTimestamp(timestamp: number, network = 1): Promi
 	return 0;
 }
 
-export async function fetchCryptoPrice(from = '', to = 'usd'): Promise<any> {
-	const	result = await performGet(`https://api.coingecko.com/api/v3/simple/price?ids=${from}&vs_currencies=${to}`);
+// export async function fetchCryptoPrice(from = '', to = 'usd'): Promise<any> {
+// 	const	result = await performGet(`https://api.coingecko.com/api/v3/simple/price?ids=${from}&vs_currencies=${to}`);
+// 	console.log(result);
 
-	if (result) {
-		return result;
-	}
-	return null;
-}
-
-// export function getProvider(chain = 1): ethers.providers.BaseProvider {
-// 	if (chain === 1) {
-// 		if (process.env.ALCHEMY_KEY) {
-// 			return new ethers.providers.AlchemyProvider('homestead', process.env.ALCHEMY_KEY);
-// 		}
-// 		return new ethers.providers.InfuraProvider('homestead', '9aa3d95b3bc440fa88ea12eaa4456161');
+// 	if (result) {
+// 		return result;
 // 	}
-// 	if (chain === 56) {
-// 		return new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org');
-// 	}
-// 	if (chain === 100) {
-// 		return new ethers.providers.JsonRpcProvider('https://rpc.gnosischain.com/');
-// 	}
-// 	if (chain === 137) {
-// 		if (process.env.ALCHEMY_KEY_POLYGON) {
-// 			return new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_POLYGON}`);
-// 		}
-// 		return new ethers.providers.JsonRpcProvider('https://rpc-mainnet.matic.network');
-// 	}
-// 	if (chain === 250) {
-// 		return new ethers.providers.JsonRpcProvider('https://rpc.ftm.tools');
-// 	}
-// 	if (chain === 1337) {
-// 		return new ethers.providers.JsonRpcProvider('http://localhost:8545');
-// 	}
-// 	if (chain === 42161) {
-// 		return new ethers.providers.JsonRpcProvider(`https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_ARBITRUM_KEY}/arbitrum/mainnet`);
-// 	}
-// 	return (new ethers.providers.AlchemyProvider('homestead', process.env.ALCHEMY_KEY));
-// }
-
-// export function getProviderURI(chain = 1): string {
-// 	if (chain === 1) {
-// 		return (`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`);
-// 	}
-// 	if (chain === 56) {
-// 		return ('https://bsc-dataseed1.defibit.io/');
-// 	}
-// 	if (chain === 100) {
-// 		return ('https://rpc.gnosischain.com/');
-// 	}
-// 	if (chain === 137) {
-// 		return (`https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_POLYGON}`);
-// 	}
-// 	if (chain === 250) {
-// 		return ('https://rpc.ftm.tools');
-// 	}
-// 	if (chain === 1337) {
-// 		return ('http://localhost:8545');
-// 	}
-// 	if (chain === 42161) {
-// 		return (`https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_ARBITRUM_KEY}/arbitrum/mainnet`);
-// 	}
-// 	return (`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`);
+// 	return null;
 // }
 
 export async function asyncForEach<T>(array: T[], callback: (item: T, index: number, array: T[]) => void): Promise<void> {
