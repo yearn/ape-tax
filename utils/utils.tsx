213fa313bba8cc49d2ed22b5d080c88b4fa720e9
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-07-30 21:12:10 +0700

    refactor: Update block timestamp util to use zod and new fetch util

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,6 +1,18 @@
+import {z} from 'zod';
 import axios from 'axios';
 
+import {fetch} from './fetch';
+
 import type {AxiosError, AxiosResponse} from 'axios';
+import type {TFetchReturn} from './fetch';
+
+export const blockTimestampResponseSchema = z.object({
+	status: z.string(),
+	message: z.string(),
+	result: z.number()
+});
+
+export type TBlockTimestampDetails = z.infer<typeof blockTimestampResponseSchema>;
 
 export const performGet = async (url: string): Promise<any> => {
 	return (
@@ -15,67 +27,42 @@ export const performGet = async (url: string): Promise<any> => {
 	);
 };
 
-export async function fetchBlockTimestamp(timestamp: number, network = 1): Promise<number> {
+export async function fetchBlockTimestamp(timestamp: number, network = 1): TFetchReturn<TBlockTimestampDetails> {
 	if (network === 250) {
-		const	result = await performGet(`https://api.ftmscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.FTMSCAN_API}`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
+		const baseURL = 'https://api.ftmscan.com/api?module=block&action=getblocknobytime&timestamp';
+
+		return fetch<TBlockTimestampDetails>({
+			endpoint: `${baseURL}=${new URLSearchParams(`${timestamp}`)}&closest=before&apikey=${process.env.FTMSCAN_API}`,
+			schema: blockTimestampResponseSchema
+		});
 	}
-	if (network === 56) {
-		const	result = await performGet(`https://api.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.BSCSCAN_API}`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
-	}
+
 	if (network === 137) {
-		const	result = await performGet(`https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.POLYGONSCAN_API}`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
+		const baseURL = 'https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp';
+
+		return fetch<TBlockTimestampDetails>({
+			endpoint: `${baseURL}=${new URLSearchParams(`${timestamp}`)}&closest=before&apikey=${process.env.POLYGONSCAN_API}`,
+			schema: blockTimestampResponseSchema
+		});
 	}
+
 	if (network === 42161) {
-		const	result = await performGet(`https://api.arbiscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.ETHERSCAN_API}`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
-	}
-	if (network === 43114) {
-		const	result = await performGet(`https://api.snowtrace.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.SNOWTRACE_API}`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
-	}
-	if (network === 100) {
-		const	result = await performGet(`https://blockscout.com/xdai/mainnet/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before`);
-		if (result) {
-			return result.result;
-		}
-		return 0;
+		const baseURL = 'https://api.arbiscan.io/api?module=block&action=getblocknobytime&timestamp';
+
+		return fetch<TBlockTimestampDetails>({
+			endpoint: `${baseURL}=${new URLSearchParams(`${timestamp}`)}&closest=before&apikey=${process.env.ETHERSCAN_API}`,
+			schema: blockTimestampResponseSchema
+		});
 	}
 
-	const	result = await performGet(`https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=${process.env.ETHERSCAN_API}`);
 
-	if (result) {
-		return result.result;
-	}
-	return 0;
+	const baseURL = 'https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp';
+				
+	return fetch<TBlockTimestampDetails>({
+		endpoint: `${baseURL}=${new URLSearchParams(`${timestamp}`)}&closest=before&apikey=${process.env.ETHERSCAN_API}`,
+		schema: blockTimestampResponseSchema
+	});
 }
-
-// export async function fetchCryptoPrice(from = '', to = 'usd'): Promise<any> {
-// 	const	result = await performGet(`https://api.coingecko.com/api/v3/simple/price?ids=${from}&vs_currencies=${to}`);
-// 	console.log(result);
-
-// 	if (result) {
-// 		return result;
-// 	}
-// 	return null;
-// }
 
 export async function asyncForEach<T>(array: T[], callback: (item: T, index: number, array: T[]) => void): Promise<void> {
 	for (let index = 0; index < array.length; index++) {
@@ -89,3 +76,13 @@ export function	parseMarkdown(markdownText: string): string {
 
 	return htmlText.trim();
 }
+
+// export async function fetchCryptoPrice(from = '', to = 'usd'): Promise<any> {
+// 	const	result = await performGet(`https://api.coingecko.com/api/v3/simple/price?ids=${from}&vs_currencies=${to}`);
+// 	console.log(result);
+
+// 	if (result) {
+// 		return result;
+// 	}
+// 	return null;
+// }
