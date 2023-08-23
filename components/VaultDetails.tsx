ca35ee1b4841d21159b19e03a29e74b9d2b1ea77
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-08-23 18:56:46 +0700

    feat: add apy oracle call, display snapshot for V3 vaults and strategies

--- components/VaultDetails.tsx
+++ components/VaultDetails.tsx
@@ -1,27 +1,51 @@
-import {Fragment, useEffect, useState} from 'react';
+import {Fragment, useCallback, useEffect, useState} from 'react';
 import ProgressChart from 'components/ProgressChart';
 import Suspense from 'components/Suspense';
+import APR_ORACLE_V3_ABI from 'utils/ABI/yAPROracleV3.abi';
 import {maxUint256} from 'viem';
 import {useNetwork} from 'wagmi';
 import useSWR from 'swr';
+import {readContract} from '@wagmi/core';
+import {toAddress} from '@yearn-finance/web-lib/utils/address';
 import {baseFetcher} from '@yearn-finance/web-lib/utils/fetchers';
+import {formatToNormalizedValue} from '@yearn-finance/web-lib/utils/format.bigNumber';
 import {formatAmount} from '@yearn-finance/web-lib/utils/format.number';
 import {isZero} from '@yearn-finance/web-lib/utils/isZero';
 
 import type {ReactElement} from 'react';
 import type {Maybe, TSpecificAPIResult, TVault, TVaultData} from 'utils/types';
+import type {TNDict} from '@yearn-finance/web-lib/types';
 
 function	VaultDetails({vault, vaultData}: {vault: TVault, vaultData: TVaultData}): ReactElement {
 	const	{chain} = useNetwork();
 	const	chainExplorer = chain?.blockExplorers?.default?.url || 'https://etherscan.io';
+	const	aprOracleForChain = (process?.env?.APR_ORACLE_V3 as TNDict<string>)[vault.CHAIN_ID];
 
 	const	{data: vaultAPYSWR, isLoading} = useSWR<Maybe<TSpecificAPIResult>>(`/api/specificApy?address=${vault?.VAULT_ADDR}&network=${vault?.CHAIN_ID}`, baseFetcher, {revalidateOnMount: true, revalidateOnReconnect: true, shouldRetryOnError: true});
 
 	const	[vaultAPY, set_vaultAPY] = useState<Maybe<TSpecificAPIResult>>(null);
+	const	[oracleAPY, set_oracleAPY] = useState<number>(0);
 
-	useEffect((): void => {
+	const	fetchOracleData = useCallback(async (): Promise<void> => {
+		if(vault.VAULT_ABI.startsWith('v3') && aprOracleForChain){
+
+			const currentAPR = await readContract({
+				abi: APR_ORACLE_V3_ABI,
+				address: toAddress(aprOracleForChain),
+				functionName: 'getCurrentApr',
+				args: [toAddress(vault.VAULT_ADDR)]
+			}) as bigint;
+
+			const apySnapshot = formatToNormalizedValue(currentAPR, 18) * 100;
+		
+			set_oracleAPY(apySnapshot);
+		}
+	}, [aprOracleForChain, vault.VAULT_ABI, vault.VAULT_ADDR]);
+
+	useEffect((): void => { 
 		set_vaultAPY(vaultAPYSWR);
-	}, [vaultAPYSWR]);
+		fetchOracleData();
+	}, [fetchOracleData, vaultAPYSWR, vaultData.totalAssets]);
 
 	return (
 		<section aria-label={'DETAILS'}>
@@ -77,32 +101,45 @@ function	VaultDetails({vault, vaultData}: {vault: TVault, vaultData: TVaultData}
 					</p>
 				</div>
 			</div>
-			<div className={`mb-4 font-mono text-sm font-medium ${vault.VAULT_STATUS === 'withdraw' || vault.CHAIN_ID === 56 ? 'hidden' : ''}`}>
-				<div>
-					<p className={'inline text-neutral-900'}>{'Gross APR (last week): '}</p>
-					<p className={'ml-3 inline text-neutral-700'}>
-						<Suspense wait={!vaultAPY && isLoading}>
-							{`${vaultAPY?.week || '-'}`}
-						</Suspense>
-					</p>
-				</div>
-				<div>
-					<p className={'inline text-neutral-900'}>{'Gross APR (last month): '}</p>
-					<p className={'ml-3 inline text-neutral-700'}>
-						<Suspense wait={!vaultAPY && isLoading}>
-							{`${vaultAPY?.month || '-'}`}
-						</Suspense>
-					</p>
-				</div>
-				<div>
-					<p className={'inline text-neutral-900'}>{'Gross APR (inception): '}</p>
-					<p className={'ml-3 inline text-neutral-700'}>
-						<Suspense wait={!vaultAPY && isLoading}>
-							{`${vaultAPY?.inception || '-'}`}
-						</Suspense>
-					</p>
-				</div>
+
+			<div className={`mb-4 font-mono text-sm font-medium ${vault.VAULT_STATUS === 'withdraw' ? 'hidden' : ''}`}>
+				{oracleAPY > 0 ? (
+					<div>
+						<p className={'inline text-neutral-900'}>{'V3 Oracle APR (current snapshot): '}</p>
+						<p className={'ml-3 inline text-neutral-700'}>
+							{`${formatAmount(oracleAPY, 2, 6)} %`}
+						</p>
+					</div>
+				) : (
+					<>
+						<div>
+							<p className={'inline text-neutral-900'}>{'Gross APR (last week): '}</p>
+							<p className={'ml-3 inline text-neutral-700'}>
+								<Suspense wait={!vaultAPY && isLoading}>
+									{`${vaultAPY?.week || '-'}`}
+								</Suspense>
+							</p>
+						</div>
+						<div>
+							<p className={'inline text-neutral-900'}>{'Gross APR (last month): '}</p>
+							<p className={'ml-3 inline text-neutral-700'}>
+								<Suspense wait={!vaultAPY && isLoading}>
+									{`${vaultAPY?.month || '-'}`}
+								</Suspense>
+							</p>
+						</div>
+						<div>
+							<p className={'inline text-neutral-900'}>{'Gross APR (inception): '}</p>
+							<p className={'ml-3 inline text-neutral-700'}>
+								<Suspense wait={!vaultAPY && isLoading}>
+									{`${vaultAPY?.inception || '-'}`}
+								</Suspense>
+							</p>
+						</div>
+					</>
+				)}
 			</div>
+			
 			<div className={'mb-4 font-mono text-sm font-medium text-neutral-700'}>
 				<div>
 					<p className={'inline text-neutral-900'}>{'Price Per Share: '}</p>
