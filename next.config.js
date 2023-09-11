703e0436f1d88a35b0651ba95db72ae98e91fef8
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-09-11 17:04:06 +0800

    feat: Add sentry for better error tracing

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa')({
 	runtimeCaching,
 	buildExcludes: [/middleware-manifest.json$/]
 });
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true'
 });
@@ -86,3 +87,42 @@ module.exports = withTM(withBundleAnalyzer(withPWA({
 		}
 	}
 })));
+
+
+
+if (process.env.NODE_ENV === 'production') {
+	const {withSentryConfig} = require('@sentry/nextjs');
+
+	module.exports = withSentryConfig(
+		module.exports,
+		{
+			// For all available options, see:
+			// https://github.com/getsentry/sentry-webpack-plugin#options
+
+			// Suppresses source map uploading logs during build
+			silent: true,
+
+			org: 'xopowo-team',
+			project: 'ape-tax'
+		},
+		{
+			// For all available options, see:
+			// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+			// Upload a larger set of source maps for prettier stack traces (increases build time)
+			widenClientFileUpload: true,
+
+			// Transpiles SDK to be compatible with IE11 (increases bundle size)
+			transpileClientSDK: true,
+
+			// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+			tunnelRoute: '/monitoring',
+
+			// Hides source maps from generated client bundles
+			hideSourceMaps: true,
+
+			// Automatically tree-shake Sentry logger statements to reduce bundle size
+			disableLogger: true
+		}
+	);
+}
